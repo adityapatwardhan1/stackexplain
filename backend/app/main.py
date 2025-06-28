9142a4b7a41f78f4f9d144ac42e074ef72e8044a
Aditya Patwardhan <adityap.thecool@gmail.com>
2025-06-28 15:16:35 -0500

    Added requirements.txt for easier installation

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from core import explain_error
+from .core import explain_error
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
